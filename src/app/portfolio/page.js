58987237601c9f089fbb1f4af5306d4d87affc51
SVN-05 <sekarnagaraj74@gmail.com>
2024-07-09 15:34:28 +0530

    modified animation for portfolio page

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -9,7 +9,16 @@ const Portfolio = () => {
   return (
     <div className="flex flex-col items-start">
       <InPageNavBar title="Portfolio" />
-      <AppContainer className={"flex flex-col pb-20 px-5 2xl:px-0"}>
+      <AppContainer
+        customInitial={{ opacity: 0, scale: 0.5 }}
+        customAnimate={{ opacity: 1, scale: 1 }}
+        customTransition={{
+          duration: 0.8,
+          delay: 0.5,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+        className={"flex flex-col pb-20 px-5 2xl:px-0"}
+      >
         <PortfolioNavBar data={portfolio_nav_options} />
         <PortfolioListContainer />
       </AppContainer>
--- src/components/container/AppContainer.js
+++ src/components/container/AppContainer.js
@@ -2,12 +2,32 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const AppContainer = ({ children, className }) => {
+const AppContainer = ({
+  children,
+  className,
+  initialX = "-120%",
+  animateX = 0,
+  transitionEase = "easeOut",
+  transitionDuration = 1,
+  customInitial = {},
+  customAnimate = {},
+  customTransition = {},
+}) => {
+  const hasInitial = Object.keys(customInitial).length > 0;
+  const hasAnimate = Object.keys(customAnimate).length > 0;
+  const hasTransition = Object.keys(customTransition).length > 0;
+
+  const initial = hasInitial ? customInitial : { x: initialX };
+  const animate = hasAnimate ? customAnimate : { x: animateX };
+  const transition = hasTransition
+    ? customTransition
+    : { ease: transitionEase, duration: transitionDuration };
+
   return (
     <motion.div
-      initial={{ x: "-120%" }}
-      animate={{ x: 0 }}
-      transition={{ ease: "easeOut", duration: 1 }}
+      initial={initial}
+      animate={animate}
+      transition={transition}
       className={`w-full mx-auto ${className} lg:max-w-[1360px]`}
     >
       {children}
