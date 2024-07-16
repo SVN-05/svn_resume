02b6e014f71dfc16a79542377d558ec949d84bdc
SVN-05 <sekarnagaraj74@gmail.com>
2024-07-16 19:29:57 +0530

    half completed mobile view drawer

--- src/components/layouts/AppNavBar/AppNav.js
+++ src/components/layouts/AppNavBar/AppNav.js
@@ -20,6 +20,7 @@ const AppNav = () => {
   const applyLightTheme = useAppStore((state) => state.applyLightTheme);
   const applyDarkTheme = useAppStore((state) => state.applyDarkTheme);
   const [isOpen, setIsOpen] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(false);
   const navBarHeight = "64px";
 
   const handleAppTheme = () => {
@@ -79,27 +80,46 @@ const AppNav = () => {
           size={20}
           className="md:hidden"
           onClick={() => {
+            if (isOpen) {
+              setShowOverlay(!showOverlay);
+            }
             setIsOpen(!isOpen);
+            if (isOpen === false) {
+              setTimeout(() => {
+                setShowOverlay(!showOverlay);
+              }, 300);
+            }
           }}
         />
       </div>
       <div
-        className={`fixed top-[77px] right-0 bg-white transition-transform flex flex-col items-start pl-6 pt-4 gap-y-5 ${
+        className={`w-full fixed top-[77px] right-0 transition-transform flex flex-col items-startgap-y-5 ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
         style={{
-          width: "200px",
           height: `calc(100vh - ${navBarHeight})`,
           transition: "transform 0.3s ease-in-out",
         }}
       >
-        {navBarOptions?.map((item2) => {
-          return (
-            <a key={item2?.text} href={item2?.link} className="flex flex-col">
-              {item2?.text}
-            </a>
-          );
-        })}
+        {showOverlay && (
+          <div className="z-10 w-full bg-black absolute top-0 right-0 bottom-0 left-0 opacity-50" />
+        )}
+        <div className="flex flex-col z-20 text-black gap-y-3 text-lg bg-white w-1/2 h-full self-end pl-6 pt-4 ">
+          {navBarOptions?.map((item2) => {
+            return (
+              <a
+                key={item2?.text}
+                href={item2?.link}
+                onClick={() => {
+                  setShowOverlay(!showOverlay);
+                  setIsOpen(!isOpen);
+                }}
+              >
+                {item2?.text}
+              </a>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
